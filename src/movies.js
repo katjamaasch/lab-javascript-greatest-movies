4de091d03514ecc7aa297d63d524d07de6f08623
Katja Maasch <katja.maasch@gmail.com>
2020-11-17 15:39:26 +0100

    covered the basics, I may return for the last problems and the bonus iterations

--- src/movies.js
+++ src/movies.js
@@ -22,7 +22,7 @@ function howManyMovies(someArray) {
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
-//Why is Jasmine telling me this: "Return average even if one of the movies does not have rate!" I tested it and it works
+//??? Why is Jasmine telling me this: "Return average even if one of the movies does not have rate!" I tested it and it works
 
 function ratesAverage(anotherArray) {
   const ratedmoviesarray = anotherArray.filter((value) => {
@@ -53,8 +53,33 @@ function dramaMoviesRate(filmarray) {
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+//??? Why is Jasmine telling me "If two movies have the same year, order them alphabetically by their title". This function does that.
+
+function orderByYear(yetAnotherMoviesArray) {
+  const clonedArray = [...yetAnotherMoviesArray];
+
+  function compare(a, b) {
+    if (a.year === b.year) {
+      function compare(a, b) {
+        return a.title - b.title;
+      }
+    } else {
+      return a.year - b.year;
+    }
+  }
+  clonedArray.sort(compare);
+  return clonedArray;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(someMovieArray) {
+  const first20Movies = someMovieArray.slice(0, 20);
+  const titlesTop20 = first20Movies.map((titleValue) => {
+    return titleValue.title;
+  });
+  return titlesTop20.sort();
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
